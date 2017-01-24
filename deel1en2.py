a4159b861762d662b1862e540caad09b07174062
Ezri-Mudde <ezri.mudde@student.hu.nl>
2017-01-24 14:10:25 +0100

    de functies voor deeltaak 2 gemaakt, voor de automatizering moet nog wel wat verandert worden

--- deel1en2.py
+++ deel1en2.py
@@ -5,8 +5,9 @@ import time
 import random
 import os
 
-def retrieve_data(test=False):
-    if test==False:
+
+def retrieve_status(test=False):
+    if test is False:
         try:
             api_url = "ipadress van de pi waar sensors aan hangen"
             response = requests.get(api_url)
@@ -15,12 +16,32 @@ def retrieve_data(test=False):
 
             return parsedResponse
         except:
+            return "Error"
+    if test:
+        return random.choice(["closed", "open", "Error"])
+
+
+def retrieve_data(test=False):
+    if test is False:
+        try:
+            api_url = "ipadress van de pi waar sensors aan hangen"
+            response = requests.get(api_url)
+
+            parsedResponse = xmltodict.parse(response.text)
+
+            save_data(parsedResponse)
+            return parsedResponse
+        except:
+            save_data(2)
             return 2
-    elif test==True:
-        return random.choice([0,1])
+    elif test:
+        data = random.choice([0, 1, 2])
+        save_data(data, True)
+        return data
+
 
 def save_data(data, test=False):
-    if test==False:
+    if test is False:
         if os.path.isfile("data.csv"):
             with open("data.csv", "a", newline="") as csvfile:
                 fieldnames = ["date","time","value"]
@@ -38,7 +59,7 @@ def save_data(data, test=False):
 
                 writer.writeheader()
                 writer.writerow({"date": date,"time": timev,"value": data})
-    elif test==True:
+    elif test:
         if os.path.isfile("testdata.csv"):
             with open("testdata.csv", "a", newline="") as csvfile:
                 fieldnames = ["date","time","value"]
@@ -56,5 +77,63 @@ def save_data(data, test=False):
 
                 writer.writeheader()
                 writer.writerow({"date": date,"time": timev,"value": data})
-for keer in range(0, 20):
-    save_data(retrieve_data(True), True)
+
+
+def execute_decision(action, test=False):
+    if test is False:
+        api_url = "ipadress van de pi waar sensors aan hangen"
+        response = requests.post(api_url, data=action)
+
+        if response.status_code == requests.codes.ok:
+            print("No problems")
+        else:
+            print("Something went wrong, a " + str(response.status_code) + " error was returned")
+    elif test:
+        print(random.choice(["No problems", "Something went wrong"]))
+
+
+def make_decision(status, test=False):
+    if test is False:
+        try:
+            data = retrieve_data()
+            if data == 0 and status == "open":
+                print("Nothing had to be done")
+            elif data == 0 and status == "closed":
+                print("The \'thing\' will open")
+                execute_decision("open")
+            elif data == 1 and status == "closed":
+                print("Nothing had to be done")
+            elif data == 1 and status == "open":
+                print("The \'thing\' will close")
+                execute_decision("close")
+            elif data == 2:
+                print("Something went wrong with retrieving the data")
+        except:
+            print("Something went wrong, we\'ll try again later")
+    if test:
+        data = retrieve_data(test)
+        if data == 0 and status == "open":
+            print("Nothing had to be done")
+        elif data == 0 and status == "closed":
+            print("The \'thing\' will open")
+            execute_decision("open", test)
+        elif data == 1 and status == "closed":
+            print("Nothing had to be done")
+        elif data == 1 and status == "open":
+            print("The \'thing\' will close")
+            execute_decision("close", test)
+        elif data == 2:
+            print("Something went wrong with retrieving the data")
+
+def main():
+    test = eval(input("Is this a test?"))
+    while True:
+        status = retrieve_status(test)
+        make_decision(status, test)
+        if test is False:
+            time.sleep(600)
+        elif test:
+            time.sleep(2)
+
+if __name__ == '__main__':
+    main()
